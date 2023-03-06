e61628264863dab4fac491ff535e5a7c32af97dc
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-06 23:23:43 +0900

    refactor: domain env  사용

--- src/services/axios/interceptor.ts
+++ src/services/axios/interceptor.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://dummyjson.com/',
+  baseURL: process.env.NEXT_PUBLIC_DOMAIN,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
