ecfb6b85500556b45b21a60f394110118434b65c
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-10 01:52:15 +0900

    refactor: searchInput form 추가 및 InputProviderValue 전체 prop 추가

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -1,24 +1,35 @@
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { FormEvent, InputHTMLAttributes } from 'react'
 
 import InputProvider, { InputProviderValue } from './context/inputProvider'
 import Input, { SUBMIT_BUTTON_KEY } from './Input'
 
-interface Props extends InputProviderValue {
-  id: string
-}
+interface Props
+  extends InputProviderValue,
+    Omit<InputHTMLAttributes<HTMLInputElement>, 'value'> {}
 /**
  * @note
  * 검색 인풋의 경우, 실시간으로 상태값을 동기화할 필요가 없기 때문에 비제어컴포넌트로 사용합니다.
  */
-const SearchInput = ({ id, submit }: Props) => {
+const SearchInput = ({
+  submit,
+  value,
+  setValue,
+  ...inputAttributes
+}: Props) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
   return (
-    <InputProvider submit={submit}>
-      <SearchInput.Input id={id}>
-        <SearchInput.SubmitButton key={SUBMIT_BUTTON_KEY}>
-          제출
-        </SearchInput.SubmitButton>
-      </SearchInput.Input>
+    <InputProvider submit={submit} value={value} setValue={setValue}>
+      <form onSubmit={handleSubmit}>
+        <SearchInput.Input {...inputAttributes}>
+          <SearchInput.SubmitButton key={SUBMIT_BUTTON_KEY}>
+            제출
+          </SearchInput.SubmitButton>
+        </SearchInput.Input>
+      </form>
     </InputProvider>
   )
 }
