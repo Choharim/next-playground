68034a38f438cb9ca03dbb09a966b49ed8f09d6b
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-10 22:07:04 +0900

    refactor: product search 기능 리팩토링 및 input 컴포넌트 적용

--- src/features/index/Search.tsx
+++ src/features/index/Search.tsx
@@ -1,15 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import styled from '@emotion/styled'
+import { useRouter } from 'next/router'
+
+import { useSearchedKeyword, useSearchKeyword } from './context/fillterProvider'
+
 import SearchInput from '@/components/input/SearchInput'
-import React from 'react'
-import { useSearchKeyword } from './context/fillterProvider'
+import InputProvider from '@/components/input/context/inputProvider'
+import { SUBMIT_BUTTON_KEY } from '@/components/input/Input'
 
 const Search = () => {
-  const searchKeyword = useSearchKeyword()
+  const rounter = useRouter()
+  const searchParams = useSearchParams()
+  const keyword = searchParams.get('q')
+  const [value, setValue] = useState('')
 
-  const handleSubmit = (value: string) => {
+  const searchKeyword = useSearchKeyword()
+  const searchedKeyword = useSearchedKeyword()
+
+  useEffect(() => {
+    if (!!keyword && !searchedKeyword) {
+      searchKeyword(keyword)
+      setValue(keyword)
+    }
+  }, [keyword, searchKeyword, searchedKeyword])
+
+  const submit = () => {
     searchKeyword(value)
+    rounter.push({ ...rounter, query: { q: value } })
   }
 
-  return <SearchInput id="search" submit={handleSubmit} />
+  return (
+    <InputProvider submit={submit} value={value} setValue={setValue}>
+      <CustomSearchInput>
+        <Button key={SUBMIT_BUTTON_KEY}>제출</Button>
+      </CustomSearchInput>
+    </InputProvider>
+  )
 }
 
 export default Search
+
+const CustomSearchInput = styled(SearchInput)`
+  display: flex;
+`
+
+const Button = styled.button`
+  border: 1px solid pink;
+`
