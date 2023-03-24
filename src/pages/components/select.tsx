9ffd2a88bced6b4ebe5d54104803ed61a9a78cb6
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-24 10:38:46 +0900

    feat: select component page

--- src/pages/components/select.tsx
+++ src/pages/components/select.tsx
@@ -1,24 +1,79 @@
+import { useState } from 'react'
 import styled from '@emotion/styled'
 
+import Select from '@/components/select/Select'
+import RowLabel from '@/components/form/RowLabel'
+import Frame from '@/features/components/Frame'
+import { SelectBasic } from '@/components/select/shared'
+import Form from '@/components/form/Form'
+import Button from '@/components/button/Button'
 import SelectProvider from '@/components/select/context/selectProvider'
-import FormSelect from '@/components/select/FormSelect'
 
-const options = [
+const ANIMAL_OPTIONS: SelectBasic['options'] = [
+  { text: '오리', value: 'duck' },
   { text: '고양이', value: 'cat' },
   { text: '강아지', value: 'dog' },
-  { text: '오리', value: 'duck' },
+]
+const FRUIT_OPTIONS: SelectBasic['options'] = [
+  { text: '바나나', value: 'banana' },
+  { text: '복숭아', value: 'peach' },
+  { text: '오렌지', value: 'orange' },
 ]
 
-const Select = () => {
+const ANIMAL_SELECT_ID = 'animals'
+
+type Form = {
+  [ANIMAL_SELECT_ID]: { value: string }
+}
+const SelectPage = () => {
+  const [selectedOption, setSelectedOption] = useState('')
+
+  console.log(selectedOption)
+
+  const submitForm = (target: Form) => {
+    console.log(target[ANIMAL_SELECT_ID]?.value)
+  }
   return (
-    <SelectProvider options={options}>
-      <CustomFormSelect placeholder="선택하세요." label="아무" name="select" />
-    </SelectProvider>
+    <>
+      <Frame title="드롭다운">
+        <Form onSubmitForm={submitForm}>
+          <Label htmlFor={ANIMAL_SELECT_ID}>동물</Label>
+
+          <SelectProvider options={ANIMAL_OPTIONS}>
+            <CustomSelect
+              id={ANIMAL_SELECT_ID}
+              name={ANIMAL_SELECT_ID}
+              placeholder="선택하세요."
+            >
+              <Select.ToggleIcon>
+                <>{'^'}</>
+              </Select.ToggleIcon>
+            </CustomSelect>
+          </SelectProvider>
+
+          <Button type="submit" variety="contain">
+            확인
+          </Button>
+        </Form>
+
+        <SelectProvider
+          options={FRUIT_OPTIONS}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+        >
+          <CustomSelect defaultValue={FRUIT_OPTIONS[2].value} />
+        </SelectProvider>
+      </Frame>
+    </>
   )
 }
 
-export default Select
+export default SelectPage
 
-const CustomFormSelect = styled(FormSelect)`
-  width: 200px;
+const Label = styled(RowLabel)`
+  ${({ theme }) => theme.font.header_4};
+`
+
+const CustomSelect = styled(Select)`
+  width: 300px;
 `
