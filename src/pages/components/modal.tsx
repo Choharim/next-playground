301ed0c92934c8e00595e01b1b07c8991e48b68d
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-28 22:57:55 +0900

    feat: 페이지 이탈 hook confirm 모달과 연결

--- src/pages/components/modal.tsx
+++ src/pages/components/modal.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled'
+import { useRouter } from 'next/router'
 
 import useConfirm from '@/hooks/useConfirm'
+import useBlockLeaving from '@/hooks/useBlockLeaving'
 
 import Frame from '@/features/components/Frame'
 import ConfirmModal from '@/components/modal/ConfirmModal'
@@ -8,26 +10,39 @@ import ModalPortal from '@/components/modal/Portal'
 import Button from '@/components/button/Button'
 
 const Modal = () => {
+  const router = useRouter()
   const { isOpen, confirm, onCancel, onConfirm } = useConfirm()
+  const { unBlock } = useBlockLeaving(showModal)
 
-  const showModal = async () => {
+  async function showModal() {
     const answer = await confirm()
     if (answer) {
       console.log('확인')
+      unBlock()
     } else {
       console.log('취소')
     }
   }
+
+  const goToBack = () => {
+    router.back()
+  }
+
   return (
     <Frame title="Confirm modal">
-      <Button variety="outline" onClick={showModal}>
-        모달 열기
-      </Button>
+      <ButtonContainer>
+        <CustomButton variety="outline" onClick={showModal}>
+          모달 열기
+        </CustomButton>
+        <CustomButton variety="outline" onClick={goToBack}>
+          뒤로
+        </CustomButton>
+      </ButtonContainer>
 
       <ModalPortal>
         <ConfirmModal isOpen={isOpen} onCancel={onCancel} onConfirm={onConfirm}>
           <ConfirmModal.Body>
-            <CustomTitle>데모 모달</CustomTitle>
+            <CustomTitle>모달 제목</CustomTitle>
             <CustomDesc>모달 설명입니다.</CustomDesc>
             {/* 추가적인 버튼 커스텀이 필요하지 않고 '취소', '확인' 버튼 나열만 할 경우 , 버튼을 전달하지 않아도 됩니다. */}
             {/* 
@@ -51,4 +66,15 @@ const CustomDesc = styled(ConfirmModal.BodyDesc)`
   color: ${({ theme }) => theme.color.grey600};
   ${({ theme }) => theme.font.body_3};
   margin-top: 5px;
+`
+
+const CustomButton = styled(Button)`
+  width: 200px;
+  height: 48px;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
 `
