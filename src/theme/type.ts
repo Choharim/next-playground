a1fa38a6b1388a62a08fe9960e1004741504210d
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-01 13:10:37 +0900

    feat: colorKey, fontKey 타입 추가

--- src/theme/type.ts
+++ src/theme/type.ts
@@ -5,9 +5,10 @@ import { SHADOW } from './shadow'
 import { Z_INDEX } from './zIndex'
 
 export type Color = typeof COLOR
-export type ColorKey = keyof typeof COLOR
+export type ColorKey = keyof Color
 
 export type Font = typeof FONT
+export type FontKey = keyof Font
 
 export type Media = typeof MEDIA
 
