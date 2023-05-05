7141fc3d6fa0b245f4033035c35d19df8a9136fe
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-05 13:48:39 +0900

    feat: requiredFields type util 추가

--- src/shared/types/narrow.ts
+++ src/shared/types/narrow.ts
@@ -6,3 +6,6 @@ export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
     [K in Keys]-?: Required<Pick<T, K>> &
       Partial<Record<Exclude<Keys, K>, undefined>>
   }[Keys]
+
+export type RequiredFields<T, U extends keyof T> = Required<Pick<T, U>> &
+  Omit<T, U>
