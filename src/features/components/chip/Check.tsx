9cc009f7874245687952205bcd190ea30cf395f3
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-05 13:50:09 +0900

    feat: chip page에 checkbox 추가

--- src/features/components/chip/Check.tsx
+++ src/features/components/chip/Check.tsx
@@ -1,23 +1,56 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 import Frame from '@/features/components/Frame'
 import CheckChip from '@/components/Chip/CheckChip'
+import Checkbox from '@/components/Chip/Checkbox'
+import Typo from '@/components/Typo'
 
 const Check = () => {
-  const [checked, setChecked] = useState(false)
+  const [checkedForChip, setCheckedForChip] = useState(false)
+  const [checkedForBox, setCheckedForBox] = useState(false)
 
-  const toggleChecked = () => {
-    setChecked((prev) => !prev)
+  const toggleCheckedForChip = () => {
+    setCheckedForChip((prev) => !prev)
   }
 
-  console.log(checked)
+  const toggleCheckedForBox = (e: ChangeEvent<HTMLInputElement>) => {
+    setCheckedForBox(e.currentTarget.checked)
+  }
+
+  console.log('checkedForBox', checkedForBox)
+  console.log('checkedForChip', checkedForChip)
 
   return (
-    <Frame title="checkChip">
-      <CheckChip onClick={toggleChecked} checked={checked}>
-        CheckChip
-      </CheckChip>
-    </Frame>
+    <>
+      <Frame title="checkChip">
+        <CheckChip onClick={toggleCheckedForChip} checked={checkedForChip}>
+          CheckChip
+        </CheckChip>
+      </Frame>
+
+      <Frame title="checkbox">
+        <Checkbox
+          id="checkbox"
+          onChange={toggleCheckedForBox}
+          checked={checkedForBox}
+          isError
+        >
+          <Typo variety="body_2" color="grey800">
+            에러 체크 박스 라벨
+          </Typo>
+        </Checkbox>
+
+        <Checkbox
+          id="checkbox"
+          onChange={toggleCheckedForBox}
+          checked={checkedForBox}
+        >
+          <Typo variety="body_2" color="grey800">
+            체크 박스 라벨
+          </Typo>
+        </Checkbox>
+      </Frame>
+    </>
   )
 }
 
