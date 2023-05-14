a99f6326d8a280805e8f7169683f6da38b9b9558
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-14 12:01:10 +0900

    refactor: spinner component styled 적용

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,48 @@
+import { ColorKey } from '@/theme/type'
+import { css, keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import { useMemo } from 'react'
+
+interface SpinnerStyle {
+  color: ColorKey
+  size: number
+}
+
+const Spinner = ({
+  color = 'primary400',
+  size = 22,
+}: Partial<SpinnerStyle>) => {
+  const styles = useMemo(
+    () => ({
+      color,
+      size,
+    }),
+    [color, size]
+  )
+
+  return <SpinnerWrapper {...styles} role="status" />
+}
+
+export default Spinner
+
+const rotation = keyframes`
+   0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`
+
+const SpinnerWrapper = styled.span<SpinnerStyle>`
+  display: inline-block;
+  ${({ size, color, theme }) => css`
+    border: ${size / 7}px solid ${theme.color[color]};
+    width: ${size}px;
+    height: ${size}px;
+  `}
+  border-bottom-color: transparent;
+  border-radius: 50%;
+
+  animation: ${rotation} 1s linear infinite;
+`
--- src/components/button/LoadingButton.tsx
+++ src/components/button/LoadingButton.tsx
@@ -2,31 +2,42 @@ import { ComponentProps, forwardRef } from 'react'
 import styled from '@emotion/styled'
 
 import Button from '.'
-import Spiner from '../Spiner'
+import Spinner from '../Spinner'
 import Flex from '../Flex'
+
+interface SpinnerStyle {
+  spinnerColor: ComponentProps<typeof Spinner>['color']
+  spinnerSize: ComponentProps<typeof Spinner>['size']
+}
 
 interface LoadingButtonProps
   extends ComponentProps<typeof Button>,
-    ComponentProps<typeof Spiner> {
+    Partial<SpinnerStyle> {
   isLoading: boolean
 }
 const LoadingButton = forwardRef<HTMLButtonElement, LoadingButtonProps>(
   (
-    { isLoading, spinerColor, spinerSize = 22, children, ...buttonAttributes },
+    {
+      isLoading,
+      spinnerColor,
+      spinnerSize = 22,
+      children,
+      ...buttonAttributes
+    },
     forwardRef
   ) => {
     return (
       <Button {...buttonAttributes} disabled={isLoading} ref={forwardRef}>
         <Flex align="center" justify="center" gap="5px">
           {isLoading ? (
-            <Spiner spinerColor={spinerColor} spinerSize={spinerSize} />
+            <Spinner color={spinnerColor} size={spinnerSize} />
           ) : (
-            <DummyBox spinerSize={spinerSize} />
+            <DummyBox spinnerSize={spinnerSize} />
           )}
 
           {children}
 
-          <DummyBox spinerSize={spinerSize} />
+          <DummyBox spinnerSize={spinnerSize} />
         </Flex>
       </Button>
     )
@@ -37,9 +48,7 @@ export default LoadingButton
 
 LoadingButton.displayName = 'LoadingButton'
 
-const DummyBox = styled(Flex)<
-  Pick<ComponentProps<typeof Spiner>, 'spinerColor' | 'spinerSize'>
->`
-  width: ${({ spinerSize }) => spinerSize}px;
-  height: ${({ spinerSize }) => spinerSize}px;
+const DummyBox = styled(Flex)<Pick<SpinnerStyle, 'spinnerSize'>>`
+  width: ${({ spinnerSize }) => spinnerSize}px;
+  height: ${({ spinnerSize }) => spinnerSize}px;
 `
