05f01326273c3076efb4728bbefa1ba5b3b66629
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-22 22:59:38 +0900

    refactor: font style 수정

--- src/styles/constants/font.ts
+++ src/styles/constants/font.ts
@@ -1,57 +1,67 @@
 export const FONT = {
   header_1: {
-    fontSize: '42px',
-    lineHeight: '1.2',
+    fontSize: '36px',
+    lineHeight: '1.1',
     fontWeight: 700,
   },
   header_2: {
     fontSize: '32px',
-    lineHeight: '1.3',
-    fontWeight: 600,
+    lineHeight: '1.2',
+    fontWeight: 700,
   },
   header_3: {
-    fontSize: '26px',
+    fontSize: '28px',
     lineHeight: '1.3',
-    fontWeight: 600,
+    fontWeight: 700,
   },
   header_4: {
+    fontSize: '24px',
+    lineHeight: '1.4',
+    fontWeight: 700,
+  },
+
+  title_1: {
     fontSize: '22px',
-    lineHeight: '1.3',
+    lineHeight: '1.5',
     fontWeight: 600,
   },
-  subtitle_1: {
+  title_2: {
     fontSize: '20px',
-    lineHeight: '1.7',
+    lineHeight: '1.6',
     fontWeight: 600,
   },
-  subtitle_2: {
+  title_3: {
     fontSize: '18px',
     lineHeight: '1.7',
     fontWeight: 600,
   },
-  subtitle_3: {
-    fontSize: '17px',
+
+  subtitle_1: {
+    fontSize: '18px',
     lineHeight: '1.7',
+    fontWeight: 500,
+  },
+  subtitle_2: {
+    fontSize: '16px',
+    lineHeight: '1.8',
+    fontWeight: 500,
+  },
+  subtitle_3: {
+    fontSize: '14px',
+    lineHeight: '1.9',
     fontWeight: 500,
   },
 
   body_1: {
-    fontSize: '17px',
+    fontSize: '16px',
     lineHeight: '1.8',
   },
   body_2: {
-    fontSize: '16px',
-    lineHeight: '1.6',
-    fontWeight: 500,
+    fontSize: '14px',
+    lineHeight: '1.9',
   },
   body_3: {
-    fontSize: '15px',
-    lineHeight: '1.6',
-    fontWeight: 500,
-  },
-  body_4: {
-    fontSize: '14px',
-    lineHeight: '1.6',
-    fontWeight: 500,
+    fontSize: '12px',
+    lineHeight: '2.0',
   },
 } as const
