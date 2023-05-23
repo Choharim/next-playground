feeaa0a5c2782ebcba56731596620ee8749979df
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-23 21:44:14 +0900

    refactor: dropdown page form 제거

--- src/pages/components/dropdown.tsx
+++ src/pages/components/dropdown.tsx
@@ -1,19 +1,12 @@
 import { useState } from 'react'
-import { BsFillTriangleFill } from 'react-icons/bs'
-import { css } from '@emotion/react'
 
 import Dropdown from '@/components/Dropdown'
-import Input from '@/components/Input'
 import Frame from '@/features/components/Frame'
-import Form, { FormData } from '@/components/Form'
 import Label from '@/components/Label'
-import Button from '@/components/Button'
 
 import { NextPageWithLayout } from '@/shared/types/layout'
 
 const ANIMAL_SELECT_ID = 'animals'
-
-const FORM_FIELDS_NAMES = [ANIMAL_SELECT_ID] as const
 
 const OPTIONS = [
   { value: 'dog', label: '강아지' },
@@ -24,62 +17,32 @@ const OPTIONS = [
 const DropdownPage: NextPageWithLayout = () => {
   const [selectedValue, setSelectedValue] = useState('')
 
-  const submitForm = (formData: FormData<typeof FORM_FIELDS_NAMES>) => {
-    console.log('formData', formData)
-    console.log('selectedValue', selectedValue)
-  }
+  console.log('selectedValue', selectedValue)
 
   return (
     <Frame title="form">
-      <Form onSubmit={submitForm} fieldNames={FORM_FIELDS_NAMES}>
-        <Label variety="header_4" htmlFor={ANIMAL_SELECT_ID}>
-          동물
-        </Label>
+      <Label variety="header_4" htmlFor={ANIMAL_SELECT_ID}>
+        동물
+      </Label>
 
-        <Dropdown
-          options={OPTIONS}
-          selectedValue={selectedValue}
-          setSelectedValue={setSelectedValue}
-          name={ANIMAL_SELECT_ID}
-          placeholder="선택하세요."
-          // defaultValue={OPTIONS[2].value}
-        >
-          <Dropdown.Trigger
-            render={({ isOpen, label }) => (
-              <Input
-                readOnly
-                value={label}
-                css={css`
-                  cursor: pointer;
-                `}
-              >
-                <BsFillTriangleFill
-                  css={
-                    isOpen
-                      ? css`
-                          transform: rotate(180deg);
-                        `
-                      : undefined
-                  }
-                />
-              </Input>
-            )}
-          />
-          <Dropdown.OptionList>
-            {({ selectedValue, options }) =>
-              options.map((option) => (
-                <Dropdown.Option key={`${option.value}`} value={option.value}>
-                  {selectedValue === option.value && '*'} {option.label}
-                </Dropdown.Option>
-              ))
-            }
-          </Dropdown.OptionList>
-        </Dropdown>
-
-        <Button type="submit" variety="contain">
-          확인
-        </Button>
-      </Form>
+      <Dropdown
+        options={OPTIONS}
+        selectedValue={selectedValue}
+        setSelectedValue={setSelectedValue}
+        name={ANIMAL_SELECT_ID}
+        placeholder="선택하세요."
+        // defaultValue={OPTIONS[2].value}
+      >
+        <Dropdown.OptionList>
+          {({ selectedValue, options }) =>
+            options.map((option) => (
+              <Dropdown.Option key={`${option.value}`} value={option.value}>
+                {selectedValue === option.value && '*'} {option.label}
+              </Dropdown.Option>
+            ))
+          }
+        </Dropdown.OptionList>
+      </Dropdown>
     </Frame>
   )
 }
